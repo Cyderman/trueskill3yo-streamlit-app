c4f6f0531ebd5b7f61960f0a138cd3ff276f51f4
Cyderman <si.james1@gmail.com>
2024-12-22 20:43:24 +0000

    Update trueskill_display.py

--- trueskill_display.py
+++ trueskill_display.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide", page_title="PhotoFinish.Live Trueskill Ratings
 data = pd.read_csv("trueskill_table.csv")
 
 # App title
-st.title("PhotoFinish.Live Trueskill Ratings: Season 22 3YOs (end of season)")
+st.title("PhotoFinish.Live Trueskill Ratings: Season 23 3YOs (week1)")
 
 # Search functionality
 search_query = st.text_input("Search by Horse Name:")
